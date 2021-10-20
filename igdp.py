126e98463820e456c6be088d8b5b39a700772495
amRadhwen <radhweneamri9631@gmail.com>
2021-10-20 21:23:09 +0100

    system informations

---
+++ igdp.py
@@ -0,0 +1,21 @@
+import instaloader as iloader
+import os
+import platform
+
+system = platform.system()
+version = platform.version()
+machine = platform.machine()
+platforme = platform.platform()
+processor = platform.processor()
+
+print("=========================================================");
+print("_____.....------=====$$$$$ IGDP $$$$$=====-----....._____")
+print("=========================================================");
+
+print("\n-> System informations:")
+print("--> System   : ", system)
+print("--> Machine  : ", machine)
+print("--> Platform : ", platforme)
+print("--> Processor: ", processor)
+
+
